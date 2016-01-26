7738ef9ba24f50fbdd68fc9fb9bd44e1bf41831a
Petr Devaikin <p.devaikin@gmail.com>
2016-01-26 20:02:37 +0100

    visibility

--- Assets/FurnitureBehaviour.cs
+++ Assets/FurnitureBehaviour.cs
@@ -5,8 +5,11 @@ public class FurnitureBehaviour : MonoBehaviour {
 	private bool selected = false;
 	// Use this for initialization
 
+	Color vis = new Color (255, 255, 0);
+	Color transp = new Color (0, 255, 255);
+
 	void Start () {
-	
+		GameObject.Find ("Cube").GetComponent<Renderer> ().material.color = transp;
 	}
 	
 	// Update is called once per frame
@@ -17,12 +20,14 @@ public class FurnitureBehaviour : MonoBehaviour {
 	public void Select() {
 		if (!selected) {
 			selected = true;
+			GameObject.Find ("Cube").GetComponent<Renderer> ().material.color = vis;
 		}
 	}
 
 	public void Deselect() {
 		if (selected) {
 			selected = false;
+			GameObject.Find ("Cube").GetComponent<Renderer> ().material.color = transp;
 		}
 	}
 
