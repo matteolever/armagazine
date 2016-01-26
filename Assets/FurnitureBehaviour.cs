90885f845c7fedb899cdbd1044fb7a71416c3b5c
Petr Devaikin <p.devaikin@gmail.com>
2016-01-26 19:35:46 +0100

    scripts

---
+++ Assets/FurnitureBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FurnitureBehaviour : MonoBehaviour {
+	private bool selected = false;
+	// Use this for initialization
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Select() {
+		if (!selected) {
+			selected = true;
+		}
+	}
+
+	public void Deselect() {
+		if (selected) {
+			selected = false;
+		}
+	}
+
+	public bool IsSelected() {
+		return selected;
+	}
+}
